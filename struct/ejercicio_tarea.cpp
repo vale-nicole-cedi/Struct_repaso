4f66048db99b1baf17b10c077ccc76890f7a86e3
valenic-UP <0282748@up.edu.mx>
2026-02-11 14:48:19 -0600

    tarea

---
+++ struct/ejercicio_tarea.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+struct Barco{
+    string nombre;
+    float largo;
+    int muelle; //# de muelle
+};
+
+struct Carro{
+    string marca;
+    int cilindros;
+    float km;
+};
+
+int main(){
+    Barco barcos[5];
+    Carro carros[4];
+
+    for (int i=0; i<5; i++){
+
+    }
+
+    return 0;
+}
