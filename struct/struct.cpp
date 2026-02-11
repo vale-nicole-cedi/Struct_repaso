6b38a826bcf462f7cf4d99b13f6dcb2a3aadfec1
valenic-UP <0282748@up.edu.mx>
2026-02-11 13:47:38 -0600

    main

--- struct/struct.cpp
+++ struct/struct.cpp
@@ -2,5 +2,19 @@
 using namespace std;
 
 int main() {
+
+	//definir la estructura
+	struct Alumno {
+		int num_lista;
+		float calif;
+		string nombre;
+	};
+
+	//declarar un "Alumno"
+	Alumno alumno1, alumno2;
+
+	//asignar un valor a la variable
+	alumno1.calif = 9.5;
+
 	return 0;
 }
